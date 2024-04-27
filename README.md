c60f1f5d42b96fd347d9eb4cdf0a689a5e3a95e3
Matan Konstantini <matanko35@gmail.com>
2024-04-27 13:30:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MatPractice
+
+My practice area for improving/revising my skills.
+Included topics: 
+- React TypeScript
+- Node
+- UI   
+
+Website by Matan Konstantini.
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center mt-5'>
+
+        <p>_________________________</p>
+        <h1 className='font1' style={{ fontSize: 58 }}>Welcome to MatPractice</h1>
+        <h2 className='font2'>A place of learning</h2>
+        <p>_________________________</p>
+      </div>
+    </>
+  )
+}
+
+export default App
