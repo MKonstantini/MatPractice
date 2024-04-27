fd6ab41131d3f85232c7f20c89e160fd686a7b1e
Matan Konstantini <matanko35@gmail.com>
2024-04-27 17:51:29 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './styles/App.scss'
 
+import Home from './components/Home'
+
+
 function App() {
 
   return (
     <>
-      <div className='text-center mt-5'>
+      <Home />
 
-        <p>_________________________</p>
-        <h1 className='font1' style={{ fontSize: 58 }}>Welcome to MatPractice</h1>
-        <h2 className='font2'>A place of learning</h2>
-        <p>_________________________</p>
-      </div>
     </>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className='home-div'>
+            <p className='mb-5'>_________________________</p>
+            <h1 className='home-title'>White Space</h1>
+            <h2 className='font2 mt-5'>An Abstract Place For Small Projects</h2>
+            <p className='mt-5'>_________________________</p>
+        </div>
+    )
+}
+
+export default Home
