d64c710230f752cf3711c2b29a31b6e2969f98a1
Matan Konstantini <matanko35@gmail.com>
2024-04-27 19:22:18 +0300

    home page updates. interactivity with the cat

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,35 @@
-function Home() {
+import React, { FunctionComponent, useState } from "react"
+
+interface HomeProps { }
+
+const Home: FunctionComponent<HomeProps> = () => {
+    const [catImgSrc, setCatImgSrc] = useState<string>('/WS_Cat.svg');
+
+    const handleMouseEnter = () => {
+        setCatImgSrc('/WS_CatActive.svg')
+    }
+    const handleMouseLeave = () => {
+        setCatImgSrc('/WS_Cat.svg')
+    }
+
     return (
         <div className='home-div'>
-            <p className='mb-5'>_________________________</p>
-            <h1 className='home-title'>White Space</h1>
-            <h2 className='font2 mt-5'>An Abstract Place For Small Projects</h2>
-            <p className='mt-5'>_________________________</p>
+            <div>
+                <p className='mb-5'>_________________________</p>
+                <h1 className='home-title mb-5'>White Space</h1>
+                <img
+                    src={catImgSrc}
+                    alt="cat"
+                    className="cat-img mb-5"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                />
+                <h2 className='font2 mb-5'>An Abstract Place For Small Projects</h2>
+                <p>_________________________</p>
+            </div>
         </div>
     )
 }
 
 export default Home
+
